dd5c27095e37bfc71dc0d23ed4c013a21d0f21be
Nikolas Charalambidis <nikolas.charalambidis@gmail.com>
2019-10-20 12:42:08 +0200

    Test pass on build

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 
-import {ScrollToTopsss} from './atoms/';
+import {ScrollToTop} from './atoms/';
 import {ApiProvider} from './utils/api';
 import {AuthProvider} from './utils/auth';
 import {Routes} from './Routes';
