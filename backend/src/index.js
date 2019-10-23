6d764e0142b3473e6f3fd753fc3fd1ed5b699907
Nikolas Charalambidis <nikolas.charalambidis@gmail.com>
2019-10-23 17:19:23 +0200

    Modularized skeleton of mocked and DB-based API.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import swagger_v1 from './swagger/v1';
 
-import users_v1 from './v1/users';
-import teams_v1 from './v1/teams'
+import router from "./routes";
+import statusError from "./error/error"
 
 dotenv.config();
 dotenv.config({path: '.env.local'});
@@ -11,25 +10,17 @@ dotenv.config({path: '.env.local'});
 const {PORT = 3001} = process.env;
 
 const api = express();
-api.listen(PORT, () => console.log(`API started at http://localhost:${PORT}!`));
+api.listen(PORT, () => console.log(`\nAPI started at http://localhost:${PORT}`));
 
 // Sanity endpoint test
 api.get('/foo', (req, res, next) => {
 	res.json({foo: 'bar'});
 });
 
-// API v1
-users_v1(api);
-teams_v1(api);
-
-// Swagger documentation
-swagger_v1(api);
+api.use(router);
 
 // Error handling
-api.use((req, res, next) => {
-	res.status(404);
-	res.json({error: '404: Not found'});
-});
+api.use(statusError(404, 'Not found'));
 
 // Initialization
 api.use((err, req, res, next) => console.error('There was an error', err));
