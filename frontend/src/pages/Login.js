26e8e32de76c0a7f83468c826918fa397bf218cb
David Voráček <david@algorim.com>
2019-10-23 16:23:31 +0200

    login - added inputs

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import {Heading, MainSection} from '../atoms/';
-import {TopNavigation} from '../organisms/TopNavigation';
+import { Heading, MainSection } from '../atoms/';
+import { TopNavigation } from '../organisms/TopNavigation';
+import { Form, Button } from "react-bootstrap";
+
 
 export function Login() {
-	return (
-		<div>
-			<TopNavigation/>
-			<MainSection>
-				<Heading>Login</Heading>
-			</MainSection>
-		</div>
-	);
+
+    const [validated, setValidated] = useState(false);
+
+    var email = "";
+    var password = "";
+
+    const onLogin = event => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        setValidated(true);
+    };
+
+    return (
+        <div>
+            <TopNavigation />
+            <MainSection>
+                <Heading>Login</Heading>
+                <Form validated={validated}>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>E-mail</Form.Label>
+                        <Form.Control required type="email" name="email" placeholder="E-mail" ref={(input) => { email = input; }} />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Heslo</Form.Label>
+                        <Form.Control required type="password" name="password" placeholder="Heslo" ref={(input) => { password = input; }} />
+                    </Form.Group>
+                    <a href="#">Zapomenuté heslo</a>
+                    <Button variant="primary" type="button" onClick={onLogin}>Pøihlásit</Button>
+                    <Button variant="secondary" href="#">Registrovat</Button>
+                </Form>
+            </MainSection>
+        </div>
+    );
 }
