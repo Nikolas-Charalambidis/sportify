ee570de5adbcb7d34f3a4177cd0691de96177037
David Voráček <david@algorim.com>
2019-10-31 13:19:07 +0100

    add players and matches to team detail

--- frontend/src/pages/TeamDetail.js
+++ frontend/src/pages/TeamDetail.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import DatePicker from "react-datepicker";
+import { registerLocale, setDefaultLocale } from "react-datepicker";
+import cs from 'date-fns/locale/cs';
+
 import "react-datepicker/dist/react-datepicker.css";
 import { Formik, Form, Field } from 'formik';
 import * as yup from 'yup';
 import { Heading, MainSection } from '../atoms';
 import { TopNavigation } from '../organisms/TopNavigation';
-import { Breadcrumb, Button } from 'react-bootstrap';
+import { Breadcrumb, Button, Col, Row } from 'react-bootstrap';
 import { Footer } from '../organisms/Footer';
+import { CardTemplate } from '../templates/CardTemplate';
+import * as Icons from '@fortawesome/free-solid-svg-icons';
+
+registerLocale('cs', cs);
 
 export function TeamDetail() {
 
@@ -18,6 +25,7 @@ export function TeamDetail() {
     const DatePickerField = ({ name, value, onChange }) => {
         return (
             <DatePicker
+                locale="cs"
                 selected={(value && new Date(value)) || null}
                 onChange={val => {
                     onChange(name, val);
@@ -29,12 +37,18 @@ export function TeamDetail() {
     // temporary CSS styles
     const subTitle = {
         display: 'flex',
-        justifyContent: 'space-between'
+        justifyContent: 'space-between',
+    };
+
+    const form = {
+        display: 'flex',
+        justifyContent: 'space-between',
+        border: '1px solid',
     };
 
     return (
         <div>          
-            <TopNavigation />
+            <TopNavigation/>
             <MainSection>
                 <Breadcrumb>
                     <Breadcrumb.Item href="/">Domů</Breadcrumb.Item>
@@ -45,7 +59,7 @@ export function TeamDetail() {
                 <div style={subTitle} >
                     <Heading size="md">Sport: název</Heading>
                     <Heading size="md">Vedoucí týmu: jméno</Heading>
-                </div>                
+                </div>  
 
                 <Formik
                     initialValues={{ type: '1', date: '' }}
@@ -60,20 +74,41 @@ export function TeamDetail() {
                         const { values, setFieldValue } = props;
                         return (
                             <Form className="mt3">
-
-                                <Field as="select" name="type">
-                                    <option value="1">Nevybráno</option>
-                                    <option value="2">Soutěže</option>
-                                    <option value="3">Zápasy</option>
-                                </Field>
-
-                                <DatePickerField name="date" value={values.date} onChange={setFieldValue} />
-                                <Button variant="secondary" type="submit">Hledat</Button>
-                                <Button variant="secondary" onClick={props.handleReset}>Reset</Button>
+                                <div style={form}>
+                                    <div>
+                                        <Field as="select" name="type">
+                                            <option value="1">Nevybráno</option>
+                                            <option value="2">Soutěže</option>
+                                            <option value="3">Zápasy</option>
+                                        </Field>
+                                        <DatePickerField name="date" value={values.date} onChange={setFieldValue} />
+                                    </div>
+                                    <div>
+                                        <Button variant="secondary" type="submit">Hledat</Button>
+                                        <Button variant="secondary" onClick={props.handleReset}>Reset</Button>
+                                    </div>
+                                </div>
                             </Form>
                         );
                     }}
                 </Formik>
+
+                <h2 className="mt-4">Soupiska hráčů</h2>
+                <Row>
+                    <CardTemplate nazev="Tony Stark" icon={Icons.faFutbol} sport="fotbal" logo="https://bit.ly/2PCKpYD" />
+                    <CardTemplate nazev="Captain America" icon={Icons.faFutbol} sport="fotbal" logo="https://bit.ly/320mLrB" />
+                    <CardTemplate nazev="Thor" icon={Icons.faFutbol} sport="fotbal" logo="https://bit.ly/2WucRx8" />
+                    <CardTemplate nazev="Starlord" icon={Icons.faHockeyPuck} sport="hokej" logo="https://bit.ly/322b4Rh" />
+                    <CardTemplate nazev="Bruce Banner" icon={Icons.faFutbol} sport="fotbal" logo="https://bit.ly/2C0BacG" />
+                </Row>
+
+                <h2 className="mt-4">Přehled zápasů</h2>
+                <Row>
+                    <CardTemplate nazev="Protivník 1" pozice="Amatérský zápas" icon={Icons.faFutbol} sport="fotbal"
+                        logo="http://bit.ly/32Z7Hfl" stav="Probíhá" />
+                    <CardTemplate nazev="Protivník 2" pozice="Soutěž" icon={Icons.faFutbol} sport="fotbal"
+                        logo="http://bit.ly/2PrDmSC" stav="Ukončen" />
+                </Row>
 
           </MainSection>
         <Footer />
