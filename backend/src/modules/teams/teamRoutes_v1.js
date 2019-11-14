86d1368bb57e445c5152e18194964770ca00fa62
jjaros587 <jjaros587@gmail.com>
2019-11-14 18:38:53 +0100

    move matches route to team routes

--- backend/src/modules/teams/teamRoutes_v1.js
+++ backend/src/modules/teams/teamRoutes_v1.js
@@ -382,6 +382,39 @@ router.delete('/:id_team', async(req, res, next) => {
 	}
 });
 
+/**
+ * @swagger
+ * /teams/{id_team}/matches:
+ *   get:
+ *     tags:
+ *       - Teams
+ *     name: Matches
+ *     summary: Get all matches by teamID
+ *     consumes: application/json
+ *     produces: application/json
+ *     parameters:
+ *       - in: path
+ *         name: id_team
+ *         description: Team ID
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: All matches of team returned
+ *       400:
+ *         description: Invalid request
+ */
+router.get('/:id_team/matches', async (req, res, next) => {
+	try {
+		const { id_team } = req.params;
+		const matches = await new TeamService(req).getMatchesByTeam(id_team);
+		res.status(200).json({ error: false, msg: 'OK', matches: matches});
+	} catch(e) {
+		next(e);
+	}
+});
+
 
 /**
  * User object Swagger definition
--- backend/src/modules/teams/teamService.js
+++ backend/src/modules/teams/teamService.js
@@ -228,4 +228,21 @@ export default class TeamService {
 		console.log("data", {individual: invidividual, competitions_aggregate: competitions_aggregate});
 		return {individual: invidividual, competitions_aggregate: competitions_aggregate};
 	}
+
+	async getMatchesByTeam(id_team){
+		const team_id = Number(id_team);
+		teamValidation.validateTeamID(team_id);
+		return this.dbConnection.query(
+			`SELECT 
+				m.id_match, m.date,
+				host.name AS host_name, guest.name AS guest_name,
+				c.name AS competition_name
+			 FROM matches as m
+			 JOIN competitions as c ON m.id_competition=c.id_competition 
+			 JOIN teams as host ON m.id_host=host.id_team 
+			 JOIN teams as guest ON m.id_guest=guest.id_team
+			 WHERE m.id_host=? OR m.id_guest=?`,
+			[team_id, team_id]
+		);
+	}
 }
