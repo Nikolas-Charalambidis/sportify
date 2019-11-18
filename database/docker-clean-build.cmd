a8a762e9f08eba81c744be8a238388250ee6e528
Nikolas Charalambidis <nikolas.charalambidis@gmail.com>
2019-11-18 19:02:59 +0100

    Funky Docker scripts

--- database/docker-clean-build.cmd
+++ database/docker-clean-build.cmd
@@ -1,1 +1,4 @@
-docker stop mysql_sportify && docker rm mysql_sportify && docker rmi mysql_sportify:latest && docker build -t mysql_sportify .
+docker stop mysql_sportify && ^
+docker rm mysql_sportify && ^
+docker rmi mysql_sportify:latest && ^
+docker build -t mysql_sportify .
